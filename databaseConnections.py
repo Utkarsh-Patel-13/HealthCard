519243752d3eaa938b2abb8b978201d3d25ac893
utkarsh <utkarshhpatel13@gmail.com>
2020-02-18 00:10:00 +0530

    Clean up.

--- databaseConnections.py
+++ databaseConnections.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import dns
 
-client = MongoClient("mongodb+srv://SPUM:srinking69@myhealthcard-1nsmr.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://SPUM:srinking69@myhealthcard-1nsmr.mongodb.net/Health?retryWrites=true&w=majority")
 health_db = client.Health
 db_user = health_db.user
 db_doc = health_db.doctor
--- testing.py
+++ testing.py
@@ -9,14 +9,15 @@ from query.UserData import create_report
 """
 add = Address("13 Prabhupark Society", "Radhanpur Road", "Mehsana", "Gujarat", "384002")
 emer = Emergency("Utkarsh", "Patel", "9586244772", "F")
-user = create_user("Priyang", "something@gmail.com", "2001-09-12", 'M', "9409481618", "888888888887", add, emer)
+user = create_user("Priyang", "something12@gmail.com", "2001-09-12", 'M', "9409481618", "888888888877", add, emer)
 #user = create_doctor("Priyang", 'M', "9409481618", "123456789032", "111111")
 print(user)
 """
-
+"""
 x = create_report("111111", "888888888888")
 print(x)
 
 
 #find_user_name("Priyang")
 #update_user("123456789023", "9586244772")
+"""
