5b3fc481e2289366f90607eb119202c287064298
utkarsh <utkarshhpatel13@gmail.com>
2020-02-20 14:14:36 +0530

    Added styles to newpatient.html

--- forms.py
+++ forms.py
@@ -16,7 +16,7 @@ class RegistrationFormUser(FlaskForm):
     Password_confirm = PasswordField("Confirm Password",
                                      validators=[DataRequired(), Length(min=6, max=15), EqualTo('Password')])
     Name = StringField("Name", validators=[DataRequired(), Length(min=2, max=55)])
-    Gender = StringField("gender", validators=[DataRequired(), Length(min=2, max=55)])
+    Gender = StringField("Gender", validators=[DataRequired(), Length(min=2, max=55)])
     ContactNo = StringField("Contact",
                              validators=[DataRequired(), Length(min=10, max=13)])
     AadharNo = StringField("Aadhar", validators=[DataRequired(), Length(min=12, max=12)])
@@ -29,7 +29,7 @@ class RegistrationFormUser(FlaskForm):
     EmergencyContactName = StringField("Emergency contact Full Name")
     EmergencyContactRelation = StringField("Emergency contact Relation")
     EmergencyContactNumber = StringField("Emergency contact PhoneNo")
-    submit = SubmitField("register")
+    submit = SubmitField(" Register ")
 
     def validate_Email(self, Email):
         user = User.objects(Email=Email.data).first()
